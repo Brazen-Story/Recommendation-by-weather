9f54ee568a1d5013d8ccbb9f388345f089b66953
Bongjun Kim <kimbong523@gmail.com>
2023-07-06 22:36:13 +0900

    실수

--- client/by-weather/src/utils/ReportRoutes.js
+++ client/by-weather/src/utils/ReportRoutes.js
@@ -1,6 +1,6 @@
-export const savetReport= `${process.env.REACT_APP_rprsnSrvr}/report/submit` //d
-export const getReportsByName= `${process.env.REACT_APP_rprsnSrvr}/report/data` //d
-export const deleteReport= `${process.env.REACT_APP_rprsnSrvr}/report/deleteItem` //d
+export const savetReport= `${process.env.REACT_APP_rprsnSrvr}/report/submit` 
+export const getReportsByName= `${process.env.REACT_APP_rprsnSrvr}/report/data`
+export const deleteReport= `${process.env.REACT_APP_rprsnSrvr}/report/deleteItem` 
 export const updateReport= `${process.env.REACT_APP_rprsnSrvr}/report/update`
 export const managerPage= `${process.env.REACT_APP_rprsnSrvr}/report/manager`
-export const findtemp= `${process.env.REACT_APP_rprsnSrvr}/report/data` //d
+export const findtemp= `${process.env.REACT_APP_rprsnSrvr}/report/data` 
--- client/by-weather/src/utils/UserRoutes.js
+++ client/by-weather/src/utils/UserRoutes.js
@@ -1,8 +1,8 @@
-export const sign = `${process.env.REACT_APP_rprsnSrvr}/user/create`//d
-export const srvlogin = `${process.env.REACT_APP_rprsnSrvr}/user/login`//d
-export const maintainLogged = `${process.env.REACT_APP_rprsnSrvr}/user/Reissuance`//d
-export const srvlogout = `${process.env.REACT_APP_rprsnSrvr}/user/logout` //d
-export const managerUserList = `${process.env.REACT_APP_rprsnSrvr}/user/user/manager` //a
+export const sign = `${process.env.REACT_APP_rprsnSrvr}/user/create`
+export const srvlogin = `${process.env.REACT_APP_rprsnSrvr}/user/login`
+export const maintainLogged = `${process.env.REACT_APP_rprsnSrvr}/user/Reissuance`
+export const srvlogout = `${process.env.REACT_APP_rprsnSrvr}/user/logout` 
+export const managerUserList = `${process.env.REACT_APP_rprsnSrvr}/user/user/manager`
 
 
 export const loginSuccessfully = `${process.env.REACT_APP_rprsnSrvr}/user/success`
