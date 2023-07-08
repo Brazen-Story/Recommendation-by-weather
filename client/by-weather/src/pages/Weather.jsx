c47f96db154c42a61e82f0a8b2bf1b220e20bb11
Bongjun Kim <kimbong523@gmail.com>
2023-07-08 23:58:16 +0900

    mysql 연결

--- client/by-weather/src/pages/Weather.jsx
+++ client/by-weather/src/pages/Weather.jsx
@@ -9,3 +9,7 @@ function Weather() {
 }
 
 export default Weather;
+
+https://yogiweather.netlify.app/
+https://www.data.go.kr/tcs/puc/selectPublicUseCaseView.do?prcuseCaseSn=1035322
+https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15056924
--- crolling/GetRegionName.py
+++ crolling/GetRegionName.py
@@ -1,11 +1,26 @@
 import sys
 import io
+import re
+import mysql.connector
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 # 콘솔의 기본 인코딩을 'utf-8'로 변경
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+# MySQL 연결 설정
+db_config = {
+    'user': 'root',
+    'password': 'kim0523',
+    'host': 'localhost',
+    'database': 'byWeather',
+    'raise_on_warnings': True
+}
+
+# MySQL 연결
+cnx = mysql.connector.connect(**db_config)
+cursor = cnx.cursor()
 
 # 크롬 드라이버 경로
 driver_path = "C:/chromedriver.exe"
@@ -36,10 +51,35 @@ try:
     # div 내부의 모든 p 태그 찾기
     paragraphs = div.find_elements("tag name", "p")
 
-    # p 태그의 텍스트 출력
+    # p 태그의 텍스트 출력 및 MySQL에 삽입
     for p in paragraphs:
-        print(p.text)
+        text = p.text
+
+        # 대괄호가 포함된 문자열 및 빈 문자열 제거
+        filtered_text = re.sub(r'\[.*?\]', '', text).strip()
+
+        # "-"가 없는 문자열 제거
+        if "-" not in filtered_text:
+            continue
+
+        # 한국어와 영어로 분리
+        english_name, korean_name = filtered_text.split(" - ")
+
+        # 출력
+        print(korean_name, english_name)
+
+        # MySQL에 삽입
+        # MySQL에 삽입
+        insert_query = "INSERT INTO region_name (English, Korean) VALUES (%s, %s)"
+        cursor.execute(insert_query, (korean_name, english_name))
+
+    # 변경사항 커밋
+    cnx.commit()
 
 finally:
+    # MySQL 연결 종료
+    cursor.close()
+    cnx.close()
+
     # WebDriver 종료
     driver.quit()
